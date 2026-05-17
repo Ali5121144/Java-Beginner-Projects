e84b614e44cf07e55c478638d56bca200c30827e
Ali Ahmad <paigham512@gmail.com>
2026-05-17 12:17:04 -0700

    Create OutputChallenge.java

---
+++ 01-Output-Challenges/OutputChallenge.java
@@ -0,0 +1,15 @@
+public class OutputChallenge {
+    public static void main(String[] args) {
+
+        System.out.print("Hello");
+        System.out.println("World!");
+
+        System.out.println("Java \"is fun!\"");
+
+        System.out.print("Start");
+        System.out.print("\tMiddle");
+        System.out.println("\tEnd");
+
+        System.out.println("C:\\Program Files\\Java");
+    }
+}
